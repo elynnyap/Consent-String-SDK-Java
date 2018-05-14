fc5c527a2ab962b06eda14efb8f8f3b09f55195d
E-Lynn Yap <elynn.yap@liveramp.com>
2018-05-14 11:58:05 -0400

    Remove binary search and refactor RangeEntry class

--- src/main/java/com/iab/gdpr/ConsentStringParser.java
+++ src/main/java/com/iab/gdpr/ConsentStringParser.java
@@ -235,24 +235,8 @@ public class ConsentStringParser {
 	}
 
 	private boolean findVendorIdInRange(int vendorId) {
-		int limit = rangeEntries.size();
-		if (limit == 0) {
-			return false;
-		}
-		int index = limit / 2;
-		while (index >= 0 && index < limit) {
-			RangeEntry entry = rangeEntries.get(index);
-			if (entry.containsVendorId(vendorId)) {
-				return true;
-			}
-			if (index == 0 || index == limit - 1) {
-				return false;
-			}
-			if (entry.idIsGreaterThanMax(vendorId)) {
-				index = (index + ((limit - index) / 2));
-			} else {
-				index = index / 2;
-			}
+	  for (RangeEntry rangeEntry : rangeEntries) {
+	  	if (rangeEntry.containsVendorId(vendorId)) return true;
 		}
 		return false;
 	}
@@ -277,33 +261,20 @@ public class ConsentStringParser {
 		/**
 		 * This class corresponds to the RangeEntry field given in the consent string specification.
 		 */
-		private final List<Integer> vendorIds = new ArrayList<Integer>();
 		private final int maxVendorId;
 		private final int minVendorId;
 
 		public RangeEntry(int vendorId) {
-			vendorIds.add(vendorId);
 			this.maxVendorId = this.minVendorId = vendorId;
 		}
 
 		public RangeEntry(int startId, int endId) {
 			this.maxVendorId = endId;
 			this.minVendorId = startId;
-			for (; startId <= endId; startId++) {
-				vendorIds.add(startId);
-			}
 		}
 
 		public boolean containsVendorId(int vendorId) {
-			return vendorIds.indexOf(vendorId) >= 0;
-		}
-
-		public boolean idIsGreaterThanMax(int vendorId) {
-			return vendorId > maxVendorId;
-		}
-
-		public boolean isIsLessThanMin(int vendorId) {
-			return vendorId < minVendorId;
+			return vendorId >= minVendorId && vendorId <= maxVendorId;
 		}
 	}
 
